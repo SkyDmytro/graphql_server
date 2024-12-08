955e59c83e5d84b02681eb94f5c73e7c2502294d
Dima <skydmytro@gmail.com>
2024-12-08 16:27:29 +0100

    first commit

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+const server = new ApolloServer({
+  typeDefs: `
+    type Query {
+      hello: String
+    }
+  `,
+  resolvers: {
+    Query: {
+      hello: () => "Hello world!",
+    },
+  },
+});
+
+async function startServer() {
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+  });
+
+  console.log(`🚀 Server ready at ${url}`);
+}
+
+startServer().catch(console.error);
